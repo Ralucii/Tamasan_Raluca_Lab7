4232a40672418806d9648d7f66aefa138cc21d4e
ralucoita <ralucaatamasan@yahoo.ro>
2022-12-20 16:08:23 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tamasan_Raluca_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Tamasan_Raluca_Lab7;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
